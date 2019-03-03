0061142abbdff014d704aa611733eb463ab9938a
tauseefvd <tauseef.nasir@venturedive.com>
2019-03-03 20:10:46 +0500

    applied some style on campgrounds page

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 var campgrounds = [
-    { name: "Saghr", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg" },
+    { name: "Saghar", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg" },
+    { name: "Ratigali", image: "https://farm9.staticflickr.com/8041/7930201874_6c17ed670a.jpg" },
+    { name: "Talagang", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg" },
+    { name: "Chakwal", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104490f1c571a2e4b4b8_340.jpg" },
+    { name: "Saghar", image: "https://farm1.staticflickr.com/130/321487195_ff34bde2f5.jpg" },
     { name: "Ratigali", image: "https://farm9.staticflickr.com/8041/7930201874_6c17ed670a.jpg" },
     { name: "Talagang", image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg" },
     { name: "Chakwal", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104490f1c571a2e4b4b8_340.jpg" },
