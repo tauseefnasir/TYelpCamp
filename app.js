a198a13652f05a07206a3a42059cca1cab39be32
Tauseef Nasir <tauseefnasir501@gmail.com>
2019-09-06 08:00:25 +0500

    adding mongo server

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ var commentRoutes      = require("./routes/comments"),
     campgroundsRoutes  = require("./routes/campgrounds"),
     indexRoutes        = require("./routes/index");
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+
+mongoose.connect("mongodb+srv://TauseefMalik:Nasir@C1@cluster0-ugpsj.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
