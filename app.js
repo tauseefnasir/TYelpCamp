adda41e705c4600f29f06d27fa929d4cc4c64700
Tauseef Nasir <tauseefnasir501@gmail.com>
2019-09-07 10:13:21 +0500

    new cluster added

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express        = require('express');
-var  app             = express(),
-     bodyParser      = require('body-parser'),
-     passport        = require("passport"),
-     methodOverride  = require("method-override"),
-     LocalStrategy   = require("passport-local"),
-     mongoose        = require('mongoose'),
-     flash           = require("connect-flash"),
-     Campground      = require("./models/campgrounds"),
-     User            = require("./models/user")
-     seedDB          = require("./seeds");
-     Comment         = require("./models/comment");
+const express = require('express');
+var app = express(),
+    bodyParser = require('body-parser'),
+    passport = require("passport"),
+    methodOverride = require("method-override"),
+    LocalStrategy = require("passport-local"),
+    mongoose = require('mongoose'),
+    flash = require("connect-flash"),
+    Campground = require("./models/campgrounds"),
+    User = require("./models/user")
+seedDB = require("./seeds");
+Comment = require("./models/comment");
 
 // requiring routes
 
-var commentRoutes      = require("./routes/comments"),
-    campgroundsRoutes  = require("./routes/campgrounds"),
-    indexRoutes        = require("./routes/index");
-    
+var commentRoutes = require("./routes/comments"),
+    campgroundsRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index");
+
 // mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+const dbUrl = 'mongodb+srv://TauseefMalik:Nasir777@cluster0-2tasy.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(dbUrl, { useNewUrlParser: true })
+    .catch(error => {
+        {
+            console.log('Database Connection issue1: ' + error)
+        }
+    });
 
-mongoose.connect("mongodb+srv://TauseefMalik:Nasir@C1@cluster0-ugpsj.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser: true});
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 app.use(methodOverride("_method"));
@@ -31,9 +37,9 @@ app.use(flash());
 
 // passport configuration
 app.use(require("express-session")({
-    secret:"secret is secret",
+    secret: "secret is secret",
     resave: false,
-    saveUninitialized:false
+    saveUninitialized: false
 }));
 
 app.use(passport.initialize());
@@ -41,10 +47,10 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.locals.currentUser = req.user;
-    res.locals.error= req.flash("error");
-    res.locals.success= req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
 
     next();
 });
@@ -54,4 +60,4 @@ app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(3000);
+app.listen(3000, ()=>console.log('application is running at 3000'));
